caa8e8d2e56ea7525d946f9dae6a833b1f829d0c
Olena-Zhyzhko <asil198609@gmail.com>
2022-08-13 00:31:25 +0300

    Settings files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import Notiflix from 'notiflix';
+
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
