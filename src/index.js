48e02dda0cfdac7e15c047e1c5342a45c11ec714
Olena-Zhyzhko <asil198609@gmail.com>
2022-08-18 14:11:35 +0300

    add moduls

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import fetchImages from './moduls/fetchImages'
+import fetchImages from './moduls/fetchImages';
+import insertGallery from './moduls/createGallery';
 
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
-const BASE_URL = "https://pixabay.com/api/";
-const API_KEY = '29231182-3aef70459dc4e66f5b04dc644';
-const LOCALSTORAGE_KEY = "fetch_key_images";
 let currentPage;
 const perPage = 40;
 btnLoadMore.classList.add("is-hidden");
@@ -21,35 +19,24 @@ btnLoadMore.classList.add("is-hidden");
 form.addEventListener('submit', onFormBtnSubmit);
 btnLoadMore.addEventListener('click', onBtnLoadMore);
 
+function onFormBtnSubmit(e) {
+    e.preventDefault();
+    cleanerImageResult();
+    currentPage = 1;
+    afterFetch(createUrl(), currentPage);
+}
 
-const afterFetch = async (url) => {
+function onBtnLoadMore() {
+    btnLoadMore.classList.add("is-hidden");
+    currentPage += 1;
+    afterFetch(createUrl(), currentPage);
+    btnLoadMore.classList.remove("is-hidden");
+}
+
+async function afterFetch(surchImages, currentPage) {
     try {
-        // fetchImages(url);
-        const responce = await axios.get(`${BASE_URL}?key=${API_KEY}&${url}`);
-        console.log(responce.data.totalHits);
-        const searchResult = responce.data.hits;
-        const totalResults = responce.data.totalHits;
-       
-        const arrayResponse = Object.keys(searchResult);
-        if (arrayResponse.length === 0) {
-            notFoundImages();
-        } else {
-            insertGallery(searchResult);
-            lightBox.refresh();
-        }
-        btnLoadMore.classList.remove("is-hidden");
-        
-        if (currentPage === 1) {
-            Notiflix.Notify.success(`Hooray! We found ${totalResults} images.`);
-        }
-
-        const maxResult = Math.ceil(totalResults / perPage);
-    console.log(maxResult);
-
-         if (maxResult === currentPage) {
-        btnLoadMore.classList.add("is-hidden");
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-    }
+        const responceData = await fetchImages(surchImages, currentPage);
+        checkResult(responceData, currentPage);
     }
     catch {error => { 
         console.log(error);
@@ -57,100 +44,39 @@ const afterFetch = async (url) => {
     }
 };
 
-function onFormBtnSubmit(e) {
-    e.preventDefault();
-    cleanerImageResult();
-    currentPage = 1;
-    const url = createUrl();
-    afterFetch(url);
-
-    // btnLoadMore.classList.remove("is-hidden");
-   
-//    if (responce.data.totalHits)
-}
-
-function onBtnLoadMore(e) {
-    btnLoadMore.classList.add("is-hidden");
-    currentPage += 1;
-    const url = createUrl();
-    // const fetchResult =
-        afterFetch(url);
-    // console.log(fetchResult.totalHits);
-    // const maxResult = Math.ceil(fetchResult.totalHits / 100);
-    // console.log(maxResult);
-   
-    btnLoadMore.classList.remove("is-hidden");
-
-}
-
 function createUrl() {
     const surchImages = form.elements.searchQuery.value;
-    // updauteInput();
-    const urlPart = `q=${surchImages}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${currentPage}`;
-    // saveFormValue();
-    return urlPart;
+    return surchImages;
 }
 
-// function saveFormValue(surchImages) {
-//     localStorage.setItem(LOCALSTORAGE_KEY, surchImages);
-// }
 
-// function updauteInput(surchImages) {
+function checkResult(responceData, currentPage) {
+        const searchResult = responceData.hits;
+        const totalResults = responceData.totalHits;
+        const maxResult = Math.ceil(totalResults / perPage);
 
-//     const savedValue = localStorage.getItem(LOCALSTORAGE_KEY);
+        const arrayResponse = Object.keys(searchResult);
+        if (arrayResponse.length === 0) {
+            notFoundImages();
+        } else {
+            if (currentPage === 1) {
+            Notiflix.Notify.success(`Hooray! We found ${totalResults} images.`);
+            }
 
-//     if (surchImages !== savedValue ) {
-//         cleanerImageResult
-//     }
-
-    // parsedValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+            insertGallery(searchResult);
+            lightBox.refresh();
+            btnLoadMore.classList.remove("is-hidden");
+        }
     
-    // autoCompleteValue(parsedValue);
-// }
-
+        if (maxResult === currentPage) {
+            btnLoadMore.classList.add("is-hidden");
+         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+}
 
 function notFoundImages() {
     Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.');
 }
-
-
-function insertGallery(responce) {
-    const galleryItem = createGallery(responce);
-    gallery.insertAdjacentHTML('beforeend', galleryItem);
-    // gallery.innerHTML = galleryItem;
-};
-
-function createGallery(galleryItems) {
-    return galleryItems.map(galleryItem => createPhotoCard(galleryItem)).join('')
-};
-        
-function createPhotoCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-    return `
-    <div class="photo-card">
-        <a class="gallery__item" href="${largeImageURL}">
-        <div class="photo__thumb">
-            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
-        </div>
-        <div class="info">
-            <p class="info-item">
-            <b>Likes </b> <br>
-            <span class="statictics">${likes}</span>
-            </p>
-            <p class="info-item">
-            <b>Views</b> <span class="statictics">${views}</span>
-            </p>
-            <p class="info-item">
-            <b>Comments</b> <span class="statictics">${comments}</span>
-            </p>
-            <p class="info-item">
-            <b>Downloads</b> <span class="statictics">${downloads}</span>
-            </p>
-        </div>
-    </a>
-    </div>
-     `};
-
-
 
 function cleanerImageResult() {
     gallery.innerHTML = "";
---
+++ src/moduls/createGallery.js
@@ -0,0 +1,37 @@
+const gallery = document.querySelector('.gallery');
+
+export default function insertGallery(responce) {
+    const galleryItem = createGallery(responce);
+    gallery.insertAdjacentHTML('beforeend', galleryItem);
+    // gallery.innerHTML = galleryItem;
+};
+
+function createGallery(galleryItems) {
+    return galleryItems.map(galleryItem => createPhotoCard(galleryItem)).join('')
+};
+        
+function createPhotoCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+    return `
+    <div class="photo-card">
+        <a class="gallery__item" href="${largeImageURL}">
+        <div class="photo__thumb">
+            <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </div>
+        <div class="info">
+            <p class="info-item">
+            <b>Likes </b> <br>
+            <span class="statictics">${likes}</span>
+            </p>
+            <p class="info-item">
+            <b>Views</b> <span class="statictics">${views}</span>
+            </p>
+            <p class="info-item">
+            <b>Comments</b> <span class="statictics">${comments}</span>
+            </p>
+            <p class="info-item">
+            <b>Downloads</b> <span class="statictics">${downloads}</span>
+            </p>
+        </div>
+    </a>
+    </div>
+     `};
