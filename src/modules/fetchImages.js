ba0a38642ad216dae36d3e477c9c425877cb3223
Olena-Zhyzhko <asil198609@gmail.com>
2022-08-19 00:05:41 +0300

    убирает импорт perPage

--- src/modules/fetchImages.js
+++ src/modules/fetchImages.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import PER_PAGE from '../index'
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = '29231182-3aef70459dc4e66f5b04dc644';
 const FILTERS = 'image_type=photo&orientation=horizontal&safesearch=true';
